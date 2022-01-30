3e843c9ac0ed989157b693e2e1525d13c1c5dcaf
Vinishaah Jeyabalan <68726836+vinishaahjeyabalan@users.noreply.github.com>
2022-01-31 01:56:58 +0800

    Add files via upload

---
+++ DIY/src/diy/DIY.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diy;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vinishaah Jeyabalan
+ */
+public class DIY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s = new Scanner (System.in);
+        double num = s.nextDouble();
+        System.out.println("The temperature in degree fahrenheit is ");
+        double cel = (num-32)/1.8;
+        System.out.printf("The temperature in degree celcius is %.2f", cel);
+        
+        
+        
+               
+    }
+    
+}
