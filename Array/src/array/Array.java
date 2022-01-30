256f0ad3e6c8559669b266cf3846e50a9bab37bb
Vinishaah Jeyabalan <68726836+vinishaahjeyabalan@users.noreply.github.com>
2022-01-31 01:50:00 +0800

    Add files via upload

---
+++ Array/src/array/Array.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+
+
+/**
+ *
+ * @author Vinishaah Jeyabalan
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random r = new Random();
+//        int num[] = new int[10];
+//        int i, highest=0, lowest=101;
+//        int even=0, odd=0;
+//        boolean isPrime = true;
+//        System.out.print("The random numbers are: ");
+//        for(i=0; i<num.length; i++){
+//            num[i] = r.nextInt(100)+1;
+//            System.out.print(num[i] + " ");
+//        }
+//        
+//        for(i=0; i<num.length; i++){
+//            
+//            if(num[i]<lowest){
+//                lowest = num[i];
+//            }
+//            else if(num[i]>highest){
+//                highest = num[i];
+//            }
+//        }
+//       
+//        System.out.print("\nOdd numbers: ");
+//        for(i=0; i<num.length; i++){
+//            if(num[i]%2!=0){
+//                System.out.print(num[i] + " ");
+//            }
+//            odd++;}
+//            
+//        System.out.print("\nEven numbers: ");
+//        for(i=0; i<num.length; i++){
+//            if(num[i]%2==0){
+//                System.out.print(num[i] + " ");
+//            }
+//            even++;}
+//        
+//        System.out.print("\nPrime numbers: ");
+//        for(i=0; i<num.length; i++){
+//            for(int j=2; j<i/2; j++){
+//                if(num[i]%j==0){
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//            if(isPrime){
+//                System.out.print(num[i] + " ");
+//            }
+//        }
+//       
+//            
+//        System.out.println(" ");
+//        System.out.println("Lowest number: " + lowest);
+//        System.out.println("Highesr number: " + highest);
+
+
+
+
+
+
+
+
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ Array/src/java/util.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+
+/**
+ *
+ * @author Vinishaah Jeyabalan
+ */
+public class util {
+
+    public static String toString(String[] split) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
