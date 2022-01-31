e4a904b354760cc99034974e89db05ae78b56b2e
Vinishaah Jeyabalan <68726836+vinishaahjeyabalan@users.noreply.github.com>
2022-01-31 23:07:28 +0800

    Add files via upload

---
+++ L1Q2/src/l1q2/L1Q2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1q2;
+
+/**
+ *
+ * @author Vinishaah Jeyabalan
+ */
+public class L1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("My name is Vinishaah A/P Jeyabalan");
+        System.out.println("My matric number is WIF190062");
+        System.out.println("I'm staying at No.2, Jalan Kulim Square 3, Taman Kulim Square, 09600 Lunas, Kedah");
+        System.out.println("My email adress is vinishaahjeyabalan@gmail.com");
+        System.out.println("My contact is 013-4647499 ");
+    }
+    
+}
