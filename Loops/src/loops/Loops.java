9d25140268c2d20f69ecaff09ab0b3c54a533d7f
Vinishaah Jeyabalan <68726836+vinishaahjeyabalan@users.noreply.github.com>
2022-01-31 23:26:01 +0800

    Add files via upload

---
+++ Loops/src/loops/Loops.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loops;
+
+/**
+ *
+ * @author Vinishaah Jeyabalan
+ */
+public class Loops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        int i=1;
+//        while(i<10) {
+//            if(i%2==0) {
+//                System.out.println(i++);
+//                
+//            }
+//            
+//        }
+
+
+
+
+          int x=80000000;
+          while(x>0) {
+              x++;
+          }
+          System.out.println("x is " + x);
+        // TODO code application logic here
+    }
+    
+}
